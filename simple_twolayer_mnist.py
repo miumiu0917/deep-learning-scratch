239884a8627a9edec8d0b43d1dfb319bf7c14ba7
miumiu0917 <miumiu917@gmail.com>
2017-05-06 09:28:51 +0000

    udpated

---
+++ simple_twolayer_mnist.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import numpy as np
+
+from common.functions import *
+from dataset.mnist import load_mnist
+
+
+class TwoLayer(object):
+  def __init__():
+
+
+def main():
+  (train_image, train_label), (test_image, test_label) = load_mnist(normalize=True, flatten=True)
+  print(train_image.shape)
+
+
+if __name__ == '__main__':
+  main()
