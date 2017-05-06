50b5b6ce1c8954d1e4756758053c1f44d3a111e4
miumiu0917 <miumiu917@gmail.com>
2017-05-06 07:40:30 +0000

    updated

--- circuit/xor_gate.py
+++ circuit/xor_gate.py
@@ -6,11 +6,4 @@ def XOR(x, y):
   bynary = [0, 1]
   if not (x in bynary and y in bynary):
     raise ValueError('Invalid arguments. x and y must be 0 or 1.')
-  
   return OR(AND(x, NOT(y)), AND(NOT(x), y))
-
-
-print(XOR(1, 1))
-print(XOR(1, 0))
-print(XOR(0, 1))
-print(XOR(0, 0))
